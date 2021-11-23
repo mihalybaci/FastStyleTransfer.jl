fc426917224207c199ff3ec74b7fef1a69e9df97
Michael Alexander <mihalybaci@gmail.com>
2021-11-23 15:20:50 -0500

    adding example of usage

---
+++ examples/transfer.jl
@@ -0,0 +1,10 @@
+
+training_data = "C:\\Users\\michael\\Desktop\\mscoco\\val2017"
+style_folder = "C:\\Users\\michael\\Desktop\\Julia\\FastStyleTransfer.jl\\images"
+style_image = "georges-lemmen_the-beach-at-heist_1891.jpg"
+style_path = joinpath(style_folder, style_image)
+
+model_path = "C:\\Users\\michael\\Desktop\\Julia\\FastStyleTransfer.jl\\models"
+batch_size = 128
+η = 0.01
+epochs = 1
